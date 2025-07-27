abf53ebfe26ab8859f8261d5097a5181d11e7a81
Shreekumar-Git <shree181203@gmail.com>
2025-07-27 16:58:38 +0530

    Add files via upload

---
+++ Smallestnum.java
@@ -0,0 +1,30 @@
+// Smallest number amongst three numbers
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        int smallest;
+
+        if (num1 <= num2 && num1 <= num3) {
+            smallest = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            smallest = num2;
+        } else {
+            smallest = num3;
+        }
+
+        System.out.println("The smallest number is: " + smallest);
+        scanner.close();
+    }
+}
